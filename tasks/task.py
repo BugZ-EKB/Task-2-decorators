394d4d371610145080984acedea2438f4720f230
BugZ-EKB <e.shorohov87@yandex.ru>
2023-08-20 16:49:27 +0400

    Solution 2

--- tasks/task.py
+++ tasks/task.py
@@ -7,7 +7,12 @@ def log(fn):
         end = time.time()
         a = end - start
         b = str(fn.__name__)
-        log_text = str(f'{b}; args: {args} ; kwargs: {kwargs}; execution time: {a:.2f} sec')
+        arg_str = ', '.join(f'{k}={v}' for k,v in zip(fn.__code__.co_varnames, args))
+        kwarg_str = ', '.join(f'{k}={v}' for k,v in kwargs.items())
+        if len(kwarg_str) > 0:
+            log_text = str(f'{b}; args: {arg_str}; kwargs: {kwarg_str}; execution time: {a:.2f} sec')
+        else:
+            log_text = str(f'{b}; args: {arg_str}; execution time: {a:.2f} sec')
         # print(log_text)
         with open('log.txt', 'w') as log_file:
             log_file.write(log_text)
