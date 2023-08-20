1681ae2309dc3277399bd6df2a53c0699553cc43
BugZ-EKB <e.shorohov87@yandex.ru>
2023-08-20 16:15:43 +0400

    Solution 1

--- tasks/task.py
+++ tasks/task.py
@@ -1,6 +1,22 @@
+import time
 
 def log(fn):
-    """
-    Add your code here or call it from here   
-    """
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        fn(*args, **kwargs)
+        end = time.time()
+        a = end - start
+        b = str(fn.__name__)
+        log_text = str(f'{b}; args: {args} ; kwargs: {kwargs}; execution time: {a:.2f} sec')
+        # print(log_text)
+        with open('log.txt', 'w') as log_file:
+            log_file.write(log_text)
+        return fn(*args, **kwargs)
+    return wrapper
     pass
+
+# @log
+# def foo(a, b, c):
+#     return a+b+c
+#
+# foo(1, 2, c=3)
